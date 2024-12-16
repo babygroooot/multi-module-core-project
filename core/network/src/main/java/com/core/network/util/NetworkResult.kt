f2403e5adf6dc0477f242856bf7331ab9a9daa33
Meng <sokmenghour123@gmail.com>
2024-12-16 11:32:37 +0700

    Added version to navigation compose library

--- core/network/src/main/java/com/core/network/util/NetworkResult.kt
+++ core/network/src/main/java/com/core/network/util/NetworkResult.kt
@@ -1,7 +1,5 @@
 package com.core.network.util
 
-import com.core.common.R
-
 sealed class NetworkResult<out T : Any, out E : Any> {
     class Success<T : Any>(val data: T) : NetworkResult<T, Nothing>()
     class Error<E : Any>(val code: Int, val message: String?, val errorData: E? = null) : NetworkResult<Nothing, E>()
@@ -30,14 +28,4 @@ suspend fun <T : Any, E : Any> NetworkResult<T, E>.onException(
     if (this is NetworkResult.Exception) {
         executable(e, code)
     }
-}
-
-fun parseHttpErrorRespond(code: Int): Int = when (code) {
-    400 -> R.string.bad_request
-    401 -> R.string.unauthorized
-    404 -> R.string.error_not_found
-    405 -> R.string.request_not_allowed
-    500 -> R.string.server_error
-    502 -> R.string.bad_gateway
-    else -> R.string.something_went_wrong
 }
